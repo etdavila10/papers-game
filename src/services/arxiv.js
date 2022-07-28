8440470b0c67aac3017cdcbdf47b6eb98dab3133
Eduardo Torres Davila <etdavila10@gmail.com>
2022-07-28 10:19:59 -0700

    Removed protocol from arxiv api

--- src/services/arxiv.js
+++ src/services/arxiv.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Arxiv IDS have the following form YYMM.number where number is five digits or four
 // 1912.04460
 // We will only be searching by Arxiv IDS
-const baseUrl = `http://export.arxiv.org/api/query?id_list`;
+const baseUrl = `//export.arxiv.org/api/query?id_list`;
 
 const getArticle = (arxivId) => (
   axios
