9eed163e7c18a9a64e14419bd936a3a5ef7112b8
Aaron Li <aaronlikai@gmail.com>
2022-09-03 17:08:33 -0500

    Button and game over styling

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -4,7 +4,7 @@ const GameOver = (props) => {
     <div>
       <h1>You Lost. Game Over!</h1>
       { props.currScore > props.bestScore 
-        ? <div>New High Score: {props.currScore}</div> 
+        ? <div><div>Your Score: {props.currScore}</div> <div>New Best!</div> </div>
         : <div><div>Your Score: {props.currScore}</div><div>Best Score: {props.bestScore}</div></div>}
       <button onClick={props.beginGame}>Start a new game?</button>
     </div>
