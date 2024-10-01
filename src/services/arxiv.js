7fc9bd4b58dc2b195ef9eb505be8cc61720b11cb
Eduardo Torres Davila <etdavila10@gmail.com>
2024-09-30 23:11:36 -0500

    Updated backend domain

--- src/services/arxiv.js
+++ src/services/arxiv.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Arxiv IDS have the following form YYMM.number where number is five digits or four
 // 1912.04460
 // We will only be searching by Arxiv IDS
-const baseUrl = `http://137.184.79.17/random`;
+const baseUrl = `https://papersapi.com/random`;
 
 const getArticles = () => (
   axios
